3ec936b4291881331f2bed1ba66038a4dcd6f606
Vladimir Tsvetanov <vladimir.tsvetanov55@gmail.com>
2020-10-14 21:35:52 +0300

    test commit

--- app/src/main/java/com/example/ubet/MainActivity.java
+++ app/src/main/java/com/example/ubet/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.ubet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTextView = findViewById(R.id.myTextView);
+        myTextView.setText("babuli");
     }
 }

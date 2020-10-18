b146d0aeaaaaa2e1309c080c144e7cff7ff8f767
Vladimir Tsvetanov <vladimir.tsvetanov55@gmail.com>
2020-10-18 23:18:31 +0300

    httpSSSS

--- app/src/main/java/com/example/ubet/Constants.java
+++ app/src/main/java/com/example/ubet/Constants.java
@@ -1,5 +1,5 @@
 package com.example.ubet;
 
 public class Constants {
-    public static final String URL = "http://my-json-server.typicode.com";
+    public static final String URL = "https://my-json-server.typicode.com";
 }
